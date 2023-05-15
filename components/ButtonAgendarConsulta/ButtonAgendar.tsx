e72b341cdcfd61183dd8e3b2120e44118b4a3acb
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-05-15 09:48:52 -0300

    animação hover button agendar consertado

--- components/ButtonAgendarConsulta/ButtonAgendar.tsx
+++ components/ButtonAgendarConsulta/ButtonAgendar.tsx
@@ -2,7 +2,7 @@ export default function AgendarConsulta() {
   return (
     <>
       <a href="#">
-        <button className="py-5 px-10 mt-5 rounded text-white btn  bg-gradient-to-r from-cyan-500 to-blue-500 font-bold">
+        <button className="py-5 px-10 mt-5 rounded text-white btn btn-animated btn-hover bg-gradient-to-r from-cyan-500 to-blue-500 font-bold">
           Agendar Avaliação
         </button>
       </a>
