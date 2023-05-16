b23e8390a668bb772997901035a8779bbf291042
beno1234 <benolopesdias@gmail.com>
2023-05-16 11:27:39 -0300

    alterado botao (beno)

--- components/ButtonAgendarConsulta/ButtonAgendar.tsx
+++ components/ButtonAgendarConsulta/ButtonAgendar.tsx
@@ -1,9 +1,9 @@
 export default function AgendarConsulta() {
   return (
     <>
-      <div >
-        <a href="#" >
-          <button className="py-5 px-10 mt-5 rounded text-white  btn-animated btn font-bold">
+      <div>
+        <a href="#">
+          <button className="btn py-5 px-10 mt-5 rounded text-white bg-red-600 relative z-10 font-bold">
             Agendar Avaliação
           </button>
         </a>
--- components/Section2/index.tsx
+++ components/Section2/index.tsx
@@ -3,11 +3,10 @@ import Image from "next/image";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import image1 from "../../assets/FACHADA-ZL_I8A1609-copiar-2FInal-copiar.jpg";
-import image2 from "../../assets/I8A2058-copiar.jpg";
-import image3 from "../../assets/consultorio.jpeg";
-import image4 from "../../assets/I8A2050-copiar.jpg";
-import image5 from "../../assets/parede.jpeg";
+import icon1 from "../../assets/icone-carteira.webp";
+import icon2 from "../../assets/icone-home.webp";
+import icon3 from "../../assets/icone-local.webp";
+import icon4 from "../../assets/icone-estrela.webp";
 
 import AgendarConsulta from "../ButtonAgendarConsulta/ButtonAgendar";
 
@@ -44,7 +43,29 @@ export const Section2 = () => {
           A mais premiada rede de clínicas odontológicas da América Latina!
         </p>
         <div className="flex flex-col gap-10 lg:gap-10 lg:flex-row lg:justify-between w-full">
-          {/* Content */}
+          <div className="flex flex-col items-center justify-center gap-3 flex-1 bg-slate-100 rounded-lg p-4 ">
+            <Image src={icon1} alt="" width={50} />
+            <h1 className="text-xl color-three font-bold">Acesso</h1>
+            <p>Aqui você pode cuidar do seu sorriso e da sua saúde bucal!</p>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-3 flex-1 bg-slate-100 rounded-lg p-4">
+            <Image src={icon2} alt="" width={50} />
+            <h1 className="text-xl color-three font-bold">Conforto</h1>
+            <p>As melhores instalações a favor do seu conforto e bem-estar!</p>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-3 flex-1 bg-slate-100 rounded-lg p-4">
+            <Image src={icon3} alt="" width={50} />
+            <h1 className="color-three text-xl font-bold ">Conveniência</h1>
+            <p>Variedade de serviços odontológicos em um só lugar!</p>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-3 flex-1 bg-slate-100 rounded-lg p-4">
+            <Image src={icon4} alt="" width={50} />
+            <h1 className="text-xl color-three font-bold">Qualidade</h1>
+            <p>
+              Qualidade, certificados e esterilização seguindo normas de
+              biossegurança.
+            </p>
+          </div>
         </div>
         <AgendarConsulta />
       </div>
